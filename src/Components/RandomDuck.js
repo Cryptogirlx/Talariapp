17916502848c111c17d943df93614301df8402b3
Annika Davis <annikadavis@gmail.com>
2021-02-19 14:55:03 +0100

    loading and message

--- src/Components/RandomDuck.js
+++ src/Components/RandomDuck.js
@@ -1,50 +1,42 @@
-import React from 'react';
-import styled from 'styled-components';
-import App from '../App';
-import '../index.css'
+import React from "react";
+import styled from "styled-components";
+import "../index.css";
 
-
-
-function GetADuck(props){
-
-    const StyleDiv= styled.div`
+function GetADuck(props) {
+  const StyleDiv = styled.div`
     background-color: #131515;
-    color: #fff;`
-    
+    color: #fff;
+  `;
 
-    const H3 = styled.h3`
+  const H3 = styled.h3`
     font-family: cinzel;
     font-size: 50px;
     letter-spacing: 0.16px;
-    margin: 24px auto 0px;
+    margin: 20% auto 0px;
     text-align: center;
-    color: #9EFFEA;
-`
-    const SubTitle = styled.p`
+    color: #9effea;
+  `;
+
+  const RuningText = styled.p`
     max-width: 450px;
-    margin: 16px auto;
+    margin: auto;
     font-family: raleway;
-    font-weight: 700;
-    `
+    font-size: 23px;
+  `;
+  console.log("answer here?", props.answer.magic.answer);
 
-    const RuningText = styled.p`
-        max-width: 450px;
-        margin: auto;
-        font-family: raleway;
-        font-size: 23px
-    `
-    console.log("goat?", props)
-
-    return(
-        <StyleDiv>
-            <H3>Congrats, you successfully sacrificed this duck... </H3>
-            <br></br>
-            <RuningText>Our collegues are working very hard to deliver your request to Mount Olympus.</RuningText>
-            <br></br>
-            <img width="500px" src={props.randomimg} />
-         
-        </StyleDiv>
-    )
+  return (
+    <StyleDiv>
+      <H3>
+        Congrats, your request has been heard by the gods, and they have
+        replied:{" "}
+      </H3>
+      <br></br>
+      <RuningText>
+        <br></br>"{props.answer.magic.answer}."
+      </RuningText>
+    </StyleDiv>
+  );
 }
 
 export default GetADuck;
